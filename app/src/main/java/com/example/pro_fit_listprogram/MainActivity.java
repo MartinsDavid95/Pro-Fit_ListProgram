2734c947e9b9fa99b47b00cd76eff49166417efa
DAM_006 <martins.david952@gmail.com>
2023-11-30 16:03:21 +0100

    favoris cliquable et haut de la liste

--- app/src/main/java/com/example/pro_fit_listprogram/MainActivity.java
+++ app/src/main/java/com/example/pro_fit_listprogram/MainActivity.java
@@ -4,44 +4,85 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rvProgram;
-    private ImageButton btnFav;
-    private boolean isFavorited = false;
-    private String[] nomProgram, description;
-    int[] imageProgram = {
-            R.drawable.program02,
-            R.drawable.program02,
-            R.drawable.program02,
-            R.drawable.program02,
-            R.drawable.program02,
-            R.drawable.program02,
-            R.drawable.program02,
-            R.drawable.program02,
-            R.drawable.program02,
-            R.drawable.program02,
-            R.drawable.program02,
-            R.drawable.program02,
-            R.drawable.program02,
-            R.drawable.program02,
-    };
+    //private ImageButton btnFav;
+    //private boolean isFavorited = false;
+   // private String[] nomProgram, description;
+    private List<ProgramModel> programs;
+//    int[] imageProgram = {
+//            R.drawable.program02,
+//            R.drawable.program02,
+//            R.drawable.program02,
+//            R.drawable.program02,
+//            R.drawable.program02,
+//            R.drawable.program02,
+//            R.drawable.program02,
+//            R.drawable.program02,
+//            R.drawable.program02,
+//            R.drawable.program02,
+//            R.drawable.program02,
+//            R.drawable.program02,
+//            R.drawable.program02,
+//            R.drawable.program02,
+//    };
 
 
     public void init(){
         rvProgram = findViewById(R.id.rvProgram);
-        nomProgram = getResources().getStringArray(R.array.nomProgram);
-        description = getResources().getStringArray(R.array.descriptions);
-        btnFav = findViewById(R.id.btn_fav);
+        //nomProgram = getResources().getStringArray(R.array.nomProgram);
+        //description = getResources().getStringArray(R.array.descriptions);
+        //btnFav = findViewById(R.id.btn_fav);
+    }
+
+    private List<ProgramModel> createProgramsList() {
+        List<ProgramModel> programList = new ArrayList<>();
+        String[] nomProgram = getResources().getStringArray(R.array.nomProgram);
+        String[] description = getResources().getStringArray(R.array.descriptions);
+        int[] imageProgram = {
+                R.drawable.program02,
+                R.drawable.program02,
+                R.drawable.program02,
+                R.drawable.program02,
+                R.drawable.program02,
+                R.drawable.program02,
+                R.drawable.program02,
+                R.drawable.program02,
+                R.drawable.program02,
+                R.drawable.program02,
+                R.drawable.program02,
+                R.drawable.program02,
+                R.drawable.program02,
+                R.drawable.program02,
+        };
+        for (int i = 0; i < nomProgram.length; i++) {
+            // Créez un nouvel objet ProgramModel pour chaque programme
+            ProgramModel program = new ProgramModel();
+            program.setNomProgram(nomProgram[i]);
+            program.setDescription(description[i]);
+            program.setImageProgram(imageProgram[i]);
+
+            // Ajoutez le programme à la liste
+            programList.add(program);
+        }
+        return programList;
     }
 
     private void fillRecyclerProgram(){
-        RecyclerProgramAdapter adapter = new RecyclerProgramAdapter(this,nomProgram,description,imageProgram);
+        //RecyclerProgramAdapter adapter = new RecyclerProgramAdapter(this,nomProgram,description,imageProgram,btnFav,programs);
+        RecyclerProgramAdapter adapter = new RecyclerProgramAdapter(this, programs);
         rvProgram.setAdapter(adapter);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         rvProgram.setLayoutManager(linearLayoutManager);
     }
 
@@ -58,25 +99,69 @@ public class MainActivity extends AppCompatActivity {
 //        });
 //    }
 
-    public void onFavButtonClick(View view) {
-        isFavorited = !isFavorited;
+//    public void onFavButtonClick(View view) {
+//        isFavorited = !isFavorited;
+//
+//        // Mettez à jour l'icône en fonction de l'état
+//        if (isFavorited) {
+//            btnFav.setImageResource(R.drawable.star_24_y);
+//        } else {
+//            btnFav.setImageResource(R.drawable.star_24_g);
+//        }
+//    }
 
-        // Mettez à jour l'icône en fonction de l'état
-        if (isFavorited) {
-            btnFav.setImageResource(R.drawable.star_24_y);
-        } else {
-            btnFav.setImageResource(R.drawable.star_24_g);
-        }
-    }
+//    public void onImageButtonClick(View view) {
+//        // Assurez-vous que l'ImageButton est non null avant d'appeler setImageResource
+//        if (btnFav != null) {
+//            // Changez l'image du bouton lorsqu'il est cliqué
+//            btnFav.setImageResource(R.drawable.star_24_y);
+//        } else {
+//            Log.e("MainActivity", "ImageButton is null");
+//        }
+//    }
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         init();
+        programs = createProgramsList();
         fillRecyclerProgram();
 
+        SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+        List<Integer> savedItemPositions = new ArrayList<>();
 
+        for (int i = 0; i < programs.size(); i++) {
+            int savedPosition = preferences.getInt("item_position_" + i, i);
+            savedItemPositions.add(savedPosition);
+        }
+
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        rvProgram.setLayoutManager(layoutManager);
+
+        for (int i = 0; i < savedItemPositions.size(); i++) {
+            layoutManager.scrollToPositionWithOffset(savedItemPositions.get(i), 0);
+        }
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Enregistrez la position de chaque élément dans les préférences partagées
+        SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        for (int i = 0; i < programs.size(); i++) {
+            editor.putInt("item_position_" + i, i);
+        }
+
+        editor.apply();
     }
 }
--- app/src/main/java/com/example/pro_fit_listprogram/RecyclerProgramAdapter.java
+++ app/src/main/java/com/example/pro_fit_listprogram/RecyclerProgramAdapter.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,20 +15,28 @@ import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class RecyclerProgramAdapter extends RecyclerView.Adapter<RecyclerProgramAdapter.MyViewHolder> {
     private Context context;
     private String[] nomProgram, descriptions;
     private int[] imageProgram;
+    private ImageButton btnFav;
+    private boolean isFavorited = false;
+    private List<ProgramModel> programs;
 
     public RecyclerProgramAdapter() {
     }
 
-    public RecyclerProgramAdapter(Context context, String[] nomProgram, String[] descriptions, int[] imageProgram) {
+    public RecyclerProgramAdapter(Context context, List<ProgramModel> programs) {
         this.context = context;
-        this.nomProgram = nomProgram;
-        this.descriptions = descriptions;
-        this.imageProgram = imageProgram;
+//        this.nomProgram = nomProgram;
+//        this.descriptions = descriptions;
+//        this.imageProgram = imageProgram;
+//        this.btnFav = btnFav;
+        this.programs = programs;
     }
+
 
     @NonNull
     @Override
@@ -39,16 +48,66 @@ public class RecyclerProgramAdapter extends RecyclerView.Adapter<RecyclerProgram
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerProgramAdapter.MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
-        holder.tvTitre.setText(nomProgram[position]);
-        holder.tvDescription.setText(descriptions[position]);
-        holder.ivProgram.setImageResource(imageProgram[position]);
+        ProgramModel program = programs.get(position);
+
+//        holder.tvTitre.setText(nomProgram[position]);
+//        holder.tvDescription.setText(descriptions[position]);
+//        holder.ivProgram.setImageResource(imageProgram[position]);
+        holder.tvTitre.setText(program.getNomProgram());
+        holder.tvDescription.setText(program.getDescription());
+        holder.ivProgram.setImageResource(program.getImageProgram());
+
+        if (program.isFavorited()) {
+            holder.btnFav.setImageResource(R.drawable.star_24_y);
+        } else {
+            holder.btnFav.setImageResource(R.drawable.star_24_g);
+        }
+        holder.btnFav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int originalPosition = position;
+                program.setFavorited(!program.isFavorited());
+
+                if (program.isFavorited()) {
+                    // Déplacez l'élément en haut de la liste
+                    programs.remove(originalPosition);
+                    programs.add(0, program);
+                }else {
+                    programs.remove(program);
+                    programs.add(originalPosition, program);
+                }
+                Log.d("RecyclerProgramAdapter", "Original Position: " + originalPosition);
+                Log.d("RecyclerProgramAdapter", "New Position: " + programs.indexOf(program));
+                notifyDataSetChanged();
+            }
+        });
+//        holder.btnFav.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                isFavorited = !isFavorited;
+////                if (isFavorited) {
+////                    holder.btnFav.setImageResource(R.drawable.star_24_y);
+////                }
+//                if (isFavorited) {
+//                    moveItemToTop(position); // Méthode pour déplacer l'élément en haut
+//                    // Informe l'adaptateur du changement
+//                    holder.btnFav.setImageResource(R.drawable.star_24_y);
+//                    notifyDataSetChanged();
+//                }else {
+//                holder.btnFav.setImageResource(R.drawable.star_24_g);
+//                }
+//            }
+//        });
         holder.mainLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), ProgramActivity.class);
-                intent.putExtra("nomProgram",nomProgram[position]);
-                intent.putExtra("description",descriptions[position]);
-                intent.putExtra("imageProgram",imageProgram[position]);
+//                intent.putExtra("nomProgram",nomProgram[position]);
+//                intent.putExtra("description",descriptions[position]);
+//                intent.putExtra("imageProgram",imageProgram[position]);
+                intent.putExtra("nomProgram", program.getNomProgram());
+                intent.putExtra("description", program.getDescription());
+                intent.putExtra("imageProgram", program.getImageProgram());
                 v.getContext().startActivity(intent);
             }
         });
@@ -57,12 +116,18 @@ public class RecyclerProgramAdapter extends RecyclerView.Adapter<RecyclerProgram
 
     @Override
     public int getItemCount() {
-        return imageProgram.length;
+        if (programs != null) {
+            return programs.size();
+        } else {
+            return 0; // ou une valeur par défaut si programs est null
+        }
+        //return imageProgram.length;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
         private ImageView ivProgram;
         private TextView tvTitre, tvDescription;
+        private ImageButton btnFav;
         private ConstraintLayout mainLayout;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -70,6 +135,35 @@ public class RecyclerProgramAdapter extends RecyclerView.Adapter<RecyclerProgram
             tvTitre = itemView.findViewById(R.id.tv_titre);
             tvDescription = itemView.findViewById(R.id.tv_description);
             mainLayout = itemView.findViewById(R.id.main_layout);
+            btnFav = itemView.findViewById(R.id.btn_fav);
         }
     }
+    private void moveItemToTop(int position) {
+        if (position > 0 && position < nomProgram.length) {
+            // Swap l'élément actuel avec l'élément à la position 0
+            swapElements(position, 0);
+            notifyDataSetChanged(); // Informe l'adaptateur du changement
+        }
+    }
+
+    private void swapElements(int fromPosition, int toPosition) {
+        String tempNomProgram = nomProgram[fromPosition];
+        String tempDescription = descriptions[fromPosition];
+        int tempImageProgram = imageProgram[fromPosition];
+
+        // Déplacez chaque élément individuellement
+        for (int i = fromPosition; i > toPosition; i--) {
+            nomProgram[i] = nomProgram[i - 1];
+            descriptions[i] = descriptions[i - 1];
+            imageProgram[i] = imageProgram[i - 1];
+        }
+
+        nomProgram[toPosition] = tempNomProgram;
+        descriptions[toPosition] = tempDescription;
+        imageProgram[toPosition] = tempImageProgram;
+    }
+
+
+
+
 }
