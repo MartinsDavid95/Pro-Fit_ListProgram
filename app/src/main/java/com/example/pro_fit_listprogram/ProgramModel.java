7affa2f6a31afd9971e785fb813914bea39191f4
DAM_006 <martins.david952@gmail.com>
2023-11-30 16:03:45 +0100

    favoris cliquable et haut de la liste

---
+++ app/src/main/java/com/example/pro_fit_listprogram/ProgramModel.java
@@ -0,0 +1,50 @@
+package com.example.pro_fit_listprogram;
+
+public class ProgramModel {
+    private String nomProgram;
+    private String description;
+    private int imageProgram;
+    private boolean isFavorited;
+
+    public ProgramModel() {
+    }
+
+    public ProgramModel(String nomProgram, String description, int imageProgram, boolean isFavorited) {
+        this.nomProgram = nomProgram;
+        this.description = description;
+        this.imageProgram = imageProgram;
+        this.isFavorited = isFavorited;
+    }
+
+    public String getNomProgram() {
+        return nomProgram;
+    }
+
+    public void setNomProgram(String nomProgram) {
+        this.nomProgram = nomProgram;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImageProgram() {
+        return imageProgram;
+    }
+
+    public void setImageProgram(int imageProgram) {
+        this.imageProgram = imageProgram;
+    }
+
+    public boolean isFavorited() {
+        return isFavorited;
+    }
+
+    public void setFavorited(boolean favorited) {
+        isFavorited = favorited;
+    }
+}
